a8f7011916e0d0c1f8a5f564fbb2c6434345f1e6
DiegoiDavid <estradadiego733@gmail.com>
2025-10-20 01:19:32 -0600

    Add files via upload

---
+++ Act8IaC/actividad5/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tf-remote-backend-0722"
+    key    = "state.tfstate"
+    region = "us-east-2"
+  }
+}
+
